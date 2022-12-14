1f10b7dd11e26057c316a2fb4743a37799a4bc00
KatarzynaBurdzy <katarzyna.anna.burdzy@gmail.com>
2022-12-14 16:54:11 +0100

    fix-unsafe-method

--- list.js
+++ list.js
@@ -4,6 +4,7 @@ const taskNameInput = document.querySelector(".task-name-input");
 const taskNoteInput = document.querySelector(".task-note-textarea");
 const unorderedList = document.querySelector("ul");
 const taskListDiv = document.querySelector(".task-list");
+const listTitle = document.querySelector(".list-title");
 const liNote = document.querySelector(".note");
 
 // Buttons
@@ -17,36 +18,26 @@ const clearInput = function () {
 };
 
 const createListEl = function (toDo, note) {
-  // Creating new elements
   const newLi = document.createElement("li");
+  const checkbox = document.createElement("input");
   const h3Text = document.createElement("span");
   const newNote = document.createElement("p");
   const btnTrash = document.createElement("button");
   const btnMore = document.createElement("button");
 
-  // Adding text to elements
   h3Text.textContent = `${toDo}`;
   newNote.textContent = `${note}`;
-  btnTrash.textContent = `delete`;
+  btnTrash.textContent = `delete_outline`;
   btnMore.textContent = `more_horiz`;
 
-  // Adding classes to elements
   newLi.classList.add("task");
   newNote.classList.add("note", "hidden");
-  btnTrash.classList.add(
-    "btn",
-    "btn-form",
-    "btn-trash",
-    "material-symbols-outlined"
-  );
-  btnMore.classList.add(
-    "btn",
-    "btn-form",
-    "btn-more",
-    "material-symbols-outlined"
-  );
+  checkbox.classList.add("checkbox");
+  checkbox.setAttribute("type", "radio");
+  h3Text.classList.add("task-name-list");
+  btnTrash.classList.add("btn", "btn-form", "btn-trash", "material-icons");
+  btnMore.classList.add("btn", "btn-form", "btn-more", "material-icons");
 
-  // Adding event listeners to buttons
   btnTrash.addEventListener("click", () => {
     if (confirm("Your task will be deleted")) {
       newLi.remove();
@@ -55,14 +46,14 @@ const createListEl = function (toDo, note) {
 
   btnMore.addEventListener("click", () => newNote.classList.toggle("hidden"));
 
-  // Appending children
+  checkbox.addEventListener("click", () => h3Text.classList.add("checked"));
+
+  newLi.appendChild(checkbox);
   newLi.appendChild(h3Text);
   newLi.appendChild(btnTrash);
   newLi.appendChild(btnMore);
   newLi.appendChild(newNote);
   unorderedList.appendChild(newLi);
-
-  console.log(newLi);
 };
 
 // Event listeners
@@ -81,11 +72,6 @@ btnAddToList.addEventListener("click", (e) => {
   if (taskNameInput.value) {
     createListEl(taskNameInput.value, taskNoteInput.value);
     clearInput();
-    taskListDiv.classList.remove("hidden");
+    listTitle.classList.remove("hidden");
   } else alert("Add task name");
 });
-
-// btnMore.addEventListener("click", function () {
-//   // console.log("fdss");
-//   liNote.classList.toggle("hidden");
-// });
